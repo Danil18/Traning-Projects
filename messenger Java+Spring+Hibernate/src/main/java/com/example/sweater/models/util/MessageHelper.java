3b85a2c6768c2d18364867bf4f4d290cc3d2454b
Danil18 <36386580+Danil18@users.noreply.github.com>
2022-10-10 04:23:29 +0300

    Delete messenger Java+Spring+Hibernate directory

--- messenger Java+Spring+Hibernate/src/main/java/com/example/sweater/models/util/MessageHelper.java
+++
@@ -1,9 +0,0 @@
-package com.example.sweater.models.util;
-
-import com.example.sweater.models.User;
-
-public abstract class MessageHelper {
-    public static String getAuthorName(User author){
-        return author != null ? author.getUsername() : "<none>";
-    }
-}
--- messenger Java+Spring+Hibernate/target/classes/db/migration/V3__Add_subscriptions_table.sql
+++
@@ -1,5 +0,0 @@
-create table user_subscriptions (
-    channel_id bigint not null references user (id),
-    subscriber_id bigint not null references user (id),
-    primary key (channel_id, subscriber_id)
-) engine=InnoDB;
