ccf7818114e86087d14515cda51a15e5a38c66c0
Danil18 <36386580+Danil18@users.noreply.github.com>
2022-10-10 04:21:07 +0300

    Delete Shop PHP directory

--- Shop PHP/MagazinSite/config/db_params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    'host' => 'localhost',
-    'dbname' => 'online_store',
-    'user' => 'root',
-    'password' => '',
-);
--- Shop PHP/MagazinSite/template/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".add-to-cart, .btn-add-to-cart").click(function () {
-        var id = $(this).attr("data-id");
-        $.post("/MagazinSite/cart/addAjax/" + id, {}, function (data) {
-            $("#cart-count").html('('+data+')');
-        });
-        return false;
-    });
-});
