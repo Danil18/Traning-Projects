aadafd2446faf078987f71f8e93dbaedf4e83818
Danil18 <36386580+Danil18@users.noreply.github.com>
2022-11-09 16:31:49 +0300

    Update Db.php

--- REST JS+PHP/Server PHP/routers/Db.php
+++ REST JS+PHP/Server PHP/routers/Db.php
@@ -4,7 +4,7 @@ class Db {
 
     public static function getConnection(){
 
-        $db = new PDO("mysql:host=localhost;dbname=lab_rest", 'root', '');
+        $db = new PDO("mysql:host=localhost;dbname=rest", 'root', '');
         // Задаем кодировку
         $db->exec("set names utf8");
 
